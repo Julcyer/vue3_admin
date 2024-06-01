e180e877af9f7afb52ab7765bdca0a1582dfcee8
Julcyer <747768259@qq.com>
2024-06-01 17:42:06 +0800

    删除

--- dist/assets/setting-DRRv7okC.js
+++
@@ -1,1 +0,0 @@
-import{K as e}from"./index-HtYYoKz9.js";const r=e("Setting",{state:()=>({fold:!1,refsh:!1})});export{r as u};
--- src/store/index.ts
+++
@@ -1,9 +0,0 @@
-//pinia仓库
-//
-//创建一个pinia实例(大仓库)(根store)，使整个应用都能够访问到定义的stores。
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-//对外暴露给入口文件main.ts安装仓库
-export default pinia
-
-// Store是一个保存：状态、业务逻辑 的实体，每个组件都可以读取、写入它。
